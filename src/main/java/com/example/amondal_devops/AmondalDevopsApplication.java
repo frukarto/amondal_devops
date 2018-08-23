57f5b6c4dd21376d4135e5e1552fa12f05ee4056
putru2017 <putru2017@github.com>
2018-08-23 10:47:28 -0400

    Initial Commit

---
+++ src/main/java/com/example/amondal_devops/AmondalDevopsApplication.java
@@ -0,0 +1,12 @@
+package com.example.amondal_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmondalDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmondalDevopsApplication.class, args);
+	}
+}
